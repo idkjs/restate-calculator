6687cb406a42f0597a6c465688afddf05c5fe654
Alain Armand <aarmand.inbox@gmail.com>
2021-03-02 10:49:33 +0100

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,41 @@
-const path = require('path');
+// const path = require('path');
+
+// module.exports = {
+//   entry: {
+//     naive: './lib/js/src/Application.bs.js'
+//   },
+//   output: {
+//     path: path.join(__dirname, "bundledOutputs"),
+//     filename: '[name].js',
+//   },
+// };
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const outputDir = path.join(__dirname, "dist/");
+
+const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-  entry: {
-    naive: './lib/js/src/Application.bs.js'
-  },
-  output: {
-    path: path.join(__dirname, "bundledOutputs"),
-    filename: '[name].js',
-  },
+    mode: isProd ? "production" : "development",
+    entry: {
+        naive: "./src/Application.bs.js"
+    },
+    output: {
+        path: outputDir,
+        filename: "index.js",
+        publicPath: "./"
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "public/index.html"
+        })
+    ],
+    devServer: {
+        compress: true,
+        contentBase: outputDir,
+        port: process.env.PORT || 8000,
+        historyApiFallback: true,
+        stats: "minimal"
+    }
 };
